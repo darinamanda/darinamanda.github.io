6dd5b15142ad4c6b5d33caf34571688d29a7adc2
Darin Amanda Zakiya <darinamandaz@gmail.com>
2020-04-02 16:09:56 +0700

    done projects and achievements, add button to top

---
+++ style.js
@@ -0,0 +1,14 @@
+var btn = $('#backtop-button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 400) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
